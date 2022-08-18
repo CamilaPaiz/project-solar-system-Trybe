c09d2918a5f95ddf413e7c045a6cbc8975bb98ad
CamilaPaiz <camilapaiz@hotmail.com>
2022-08-18 16:15:48 -0300

    requisitos 3 e 4

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Title from './Title';
 
 export default function SolarSystem() {
-  return <div data-testid="solar-system" />;
+  return (
+    <div data-testid="solar-system">
+      <Title headline="Planetas" />
+    </div>
+  );
 }
